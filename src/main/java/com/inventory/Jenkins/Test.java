bd0b3f12fa28029877b3ce8a45175d9a551fe8d5
umeshkumar <ukm3423@gmail.com>
2024-07-03 13:32:11 +0530

    some changes on test.java file

--- src/main/java/com/inventory/Jenkins/Test.java
+++ src/main/java/com/inventory/Jenkins/Test.java
@@ -1,5 +1,17 @@
 package com.inventory.Jenkins;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Test {
     
+    private Long id; 
+    private String name;
+    private String description;
 }
